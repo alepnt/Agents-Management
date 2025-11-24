06fdc31ffdd3ac9db2c55b7637a2c35e9ec81500
alepnt <pontiggia2001@icloud.com>
2025-11-24 21:41:43 +0100

    Add serialVersionUID to client exceptions

--- client/src/main/java/com/example/client/service/BackendCommunicationException.java
+++ client/src/main/java/com/example/client/service/BackendCommunicationException.java
@@ -2,6 +2,8 @@ package com.example.client.service;
 
 public class BackendCommunicationException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
     public BackendCommunicationException(String message, Throwable cause) {
         super(message, cause);
     }
