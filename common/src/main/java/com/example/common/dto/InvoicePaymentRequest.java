db76edcee8cefcbff302a383c4c236617b238ad5
alepnt <pontiggia2001@icloud.com>
2025-11-24 23:17:09 +0100

    Add id support to invoice payment request DTO

--- common/src/main/java/com/example/common/dto/InvoicePaymentRequest.java
+++ common/src/main/java/com/example/common/dto/InvoicePaymentRequest.java
@@ -2,12 +2,14 @@ package com.example.common.dto;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
 
 /**
  * Payload per registrare un pagamento di fattura.
  */
 public class InvoicePaymentRequest {
 
+    private Long id;
     private LocalDate paymentDate;
     private BigDecimal amountPaid;
 
@@ -17,6 +19,14 @@ public class InvoicePaymentRequest {
     public InvoicePaymentRequest(LocalDate paymentDate, BigDecimal amountPaid) {
         this.paymentDate = paymentDate;
         this.amountPaid = amountPaid;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public LocalDate getPaymentDate() {
@@ -33,5 +43,22 @@ public class InvoicePaymentRequest {
 
     public void setAmountPaid(BigDecimal amountPaid) {
         this.amountPaid = amountPaid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        InvoicePaymentRequest that = (InvoicePaymentRequest) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
