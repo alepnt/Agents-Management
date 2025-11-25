7e8d37f4d357336da9f4c7caec8af15666d73df0
alepnt <pontiggia2001@icloud.com>
2025-11-25 22:17:18 +0100

    Messaggio del commit

--- server/target/classes/db/migration/V5__seed_roles_and_teams.sql
+++ server/target/classes/db/migration/V5__seed_roles_and_teams.sql
@@ -1,27 +1,27 @@
-INSERT INTO roles (name)
+INSERT INTO "roles" ("name")
 SELECT 'Amministratore'
-WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'Amministratore');
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "name" = 'Amministratore');
 
-INSERT INTO roles (name)
+INSERT INTO "roles" ("name")
 SELECT 'Responsabile Team'
-WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'Responsabile Team');
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "name" = 'Responsabile Team');
 
-INSERT INTO roles (name)
+INSERT INTO "roles" ("name")
 SELECT 'Back Office'
-WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'Back Office');
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "name" = 'Back Office');
 
-INSERT INTO teams (name)
+INSERT INTO "teams" ("name")
 SELECT 'Vendite Nord'
-WHERE NOT EXISTS (SELECT 1 FROM teams WHERE name = 'Vendite Nord');
+WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "name" = 'Vendite Nord');
 
-INSERT INTO teams (name)
+INSERT INTO "teams" ("name")
 SELECT 'Vendite Sud'
-WHERE NOT EXISTS (SELECT 1 FROM teams WHERE name = 'Vendite Sud');
+WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "name" = 'Vendite Sud');
 
-INSERT INTO teams (name)
+INSERT INTO "teams" ("name")
 SELECT 'Marketing'
-WHERE NOT EXISTS (SELECT 1 FROM teams WHERE name = 'Marketing');
+WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "name" = 'Marketing');
 
-INSERT INTO teams (name)
+INSERT INTO "teams" ("name")
 SELECT 'Supporto Clienti'
-WHERE NOT EXISTS (SELECT 1 FROM teams WHERE name = 'Supporto Clienti');
+WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "name" = 'Supporto Clienti');
