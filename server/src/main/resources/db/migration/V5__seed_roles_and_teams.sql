4ac211ef725828f788d22b1de1b301e07142caac
alepnt <pontiggia2001@icloud.com>
2025-12-03 00:34:30 +0100

    Quote identifiers in migrations for H2

--- server/src/main/resources/db/migration/V5__seed_roles_and_teams.sql
+++ server/src/main/resources/db/migration/V5__seed_roles_and_teams.sql
@@ -1,27 +1,27 @@
-INSERT INTO roles (name)
+INSERT INTO "roles" ("NAME")
 SELECT 'Amministratore'
-WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'Amministratore');
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "NAME" = 'Amministratore');
 
-INSERT INTO roles (name)
+INSERT INTO "roles" ("NAME")
 SELECT 'Responsabile Team'
-WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'Responsabile Team');
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "NAME" = 'Responsabile Team');
 
-INSERT INTO roles (name)
+INSERT INTO "roles" ("NAME")
 SELECT 'Back Office'
-WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'Back Office');
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "NAME" = 'Back Office');
 
-INSERT INTO teams (name)
+INSERT INTO "teams" ("NAME")
 SELECT 'Vendite Nord'
-WHERE NOT EXISTS (SELECT 1 FROM teams WHERE name = 'Vendite Nord');
+WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "NAME" = 'Vendite Nord');
 
-INSERT INTO teams (name)
+INSERT INTO "teams" ("NAME")
 SELECT 'Vendite Sud'
-WHERE NOT EXISTS (SELECT 1 FROM teams WHERE name = 'Vendite Sud');
+WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "NAME" = 'Vendite Sud');
 
-INSERT INTO teams (name)
+INSERT INTO "teams" ("NAME")
 SELECT 'Marketing'
-WHERE NOT EXISTS (SELECT 1 FROM teams WHERE name = 'Marketing');
+WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "NAME" = 'Marketing');
 
-INSERT INTO teams (name)
+INSERT INTO "teams" ("NAME")
 SELECT 'Supporto Clienti'
-WHERE NOT EXISTS (SELECT 1 FROM teams WHERE name = 'Supporto Clienti');
+WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "NAME" = 'Supporto Clienti');
