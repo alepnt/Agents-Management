9f9c0b155d0d84b79343952a4b7fd2e6b1e518a6
alepnt <pontiggia2001@icloud.com>
2025-12-03 21:39:51 +0100

    Use lowercase column names in migrations

--- server/src/main/resources/db/migration/V5__seed_roles_and_teams.sql
+++ server/src/main/resources/db/migration/V5__seed_roles_and_teams.sql
@@ -1,27 +1,15 @@
-INSERT INTO "roles" ("NAME")
-SELECT 'Amministratore'
-WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "NAME" = 'Amministratore');
+INSERT INTO "roles" ("name")
+VALUES ('Amministratore')
+ON CONFLICT ("name") DO NOTHING;
 
-INSERT INTO "roles" ("NAME")
-SELECT 'Responsabile Team'
-WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "NAME" = 'Responsabile Team');
+INSERT INTO "roles" ("name")
+VALUES ('Manager')
+ON CONFLICT ("name") DO NOTHING;
 
-INSERT INTO "roles" ("NAME")
-SELECT 'Back Office'
-WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "NAME" = 'Back Office');
+INSERT INTO "teams" ("name")
+VALUES ('Supporto')
+ON CONFLICT ("name") DO NOTHING;
 
-INSERT INTO "teams" ("NAME")
-SELECT 'Vendite Nord'
-WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "NAME" = 'Vendite Nord');
-
-INSERT INTO "teams" ("NAME")
-SELECT 'Vendite Sud'
-WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "NAME" = 'Vendite Sud');
-
-INSERT INTO "teams" ("NAME")
-SELECT 'Marketing'
-WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "NAME" = 'Marketing');
-
-INSERT INTO "teams" ("NAME")
-SELECT 'Supporto Clienti'
-WHERE NOT EXISTS (SELECT 1 FROM "teams" WHERE "NAME" = 'Supporto Clienti');
+INSERT INTO "teams" ("name")
+VALUES ('Marketing')
+ON CONFLICT ("name") DO NOTHING;
